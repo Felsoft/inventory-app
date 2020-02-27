65aaf2827f105de6715d61497d8eb8bad1bf9696
Felsoft <felixchoto@gmail.com>
2020-02-27 23:29:38 +0200

    initial-commit-felix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { 
+  Component,
+  EventEmitter 
+} from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-app';
+  //title = 'inventory-app';
+  products: Product[];
+  
+  constructor(){
+  // let newProduct = new Product(
+  //     'Big Fela',
+  //     'Snooker',
+  //     '/assets/images/products/GSlogo.png',
+  //     ['Accessories', 'SnookerTables'],
+  //     750.29
+  //     );
+
+      this.products = [
+        new Product(
+          'Big Fela',
+          'Snooker',
+          '/assets/images/products/GSlogo.png',
+          ['Accessories', 'SnookerTables'],
+          750.29),
+
+        new Product(
+          'Jerina Short Skirts',
+          'Lady Mini',
+          '/assets/images/products/GSlogo.png',
+          ['Ladies', 'Skirts'],
+          800.99),
+
+        new Product(
+         'Hero',
+         'Toys',
+         '/assets/images/products/GSlogo.png',
+         ['Children', 'Boys', 'Toys'],
+         100.29),
+      ];
+    }
+    
+    productWasSelected(product: Product): void {
+       console.log('Product clicked: ', product);
+    }
 }
